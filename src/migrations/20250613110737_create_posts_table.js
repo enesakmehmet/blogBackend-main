3c286c3e45b1218a2759f8eb52f32b310288f474
Enes Akmehmet <enesakmehmet7@gmail.com>
2025-06-15 16:53:34 +0300

    yeni içerikler

--- src/migrations/20250613110737_create_posts_table.js
+++ src/migrations/20250613110737_create_posts_table.js
@@ -1,12 +1,12 @@
 export async function up(knex) {
   await knex.schema.createTable("posts", (table) => {
     table.increments("id").primary();
-    table.string('title').notNullable();
-    table.text('content').notNullable();
-    table.integer('category_id').references('id').inTable('categories').onDelete('cascade');
-    table.timestamp('created_id').defaultTo(knex.fn.now());
-    table.timestamp('published_at').nullable();
-    table.timestamp('deleted_at').nullable();
+    table.string('title').notNullable();  //başlık
+    table.text('content').notNullable(); // içerik
+    table.integer('category_id').references('id').inTable('categories').onDelete('cascade');  // Kategori ile ilişkilendirme
+    table.timestamp('created_id').defaultTo(knex.fn.now()); // Oluşturulma zamanı
+    table.timestamp('published_at').nullable();    // Yayınlanma zamanı
+    table.timestamp('deleted_at').nullable();  // delete 
 
     });
 
--- src/migrations/20250613110808_create_comments_table.js
+++ src/migrations/20250613110808_create_comments_table.js
@@ -1,7 +1,7 @@
 export async function up(knex) {
     await knex.schema.createTable("comments", (table) => {
-        table.increments("id").primary();
-        table.text('content').notNullable();
+        table.increments("id").primary();  // yorum ıd
+        table.text('content').notNullable();  // yorum metni
         table.integer('post_id').references('id').inTable('posts').onDelete('cascade');
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table.string('commenter_name').nullable();
